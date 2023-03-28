1594b338f33131ebe30100fc49eaaebc8e99da5f
jefferson545 <jefferson.avelar@catolica.edu.sv>
2023-03-28 11:14:38 -0600

    avance4

--- app/src/main/java/com/example/apppaneles/MainActivity.java
+++ app/src/main/java/com/example/apppaneles/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.apppaneles;
 
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.content.Intent;
+import android.os.Handler;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        int DELAY_TIME=5000;
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                // Crea un intent para abrir la segunda actividad
+                Intent intent = new Intent(MainActivity.this, activity_login.class);
+                startActivity(intent);
+
+                // Cierra la actividad actual para que el usuario no pueda volver a ella presionando el botón de retroceso
+                finish();
+            }
+        }, DELAY_TIME);
     }
 }
---
+++ app/src/main/java/com/example/apppaneles/activity_inicioseci.java
@@ -0,0 +1,13 @@
+package com.example.apppaneles;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class activity_inicioseci extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_inicioseci);
+    }
+}
